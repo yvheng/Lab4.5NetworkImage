edf43a2b15e04da18564ee101bcf44c3b256bc85
Yv Heng <kongyh-wa15@student.tarc.edu.my>
2017-12-18 13:29:25 +0800

    Initial commit

---
+++ app/src/main/java/com/example/taruc/lab45networkimage/ImageDataContract.java
@@ -0,0 +1,8 @@
+package com.example.taruc.lab45networkimage;
+
+
+public class ImageDataContract {
+    public static final String TABLE_NAME = "album";
+    public static final String COLUMN_ID = "id";
+    public static final String COLUMN_IMAGE="image";
+}
